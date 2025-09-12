01b9355d0a6a593f252c50a6bdca0f86e1a0f4b6
Jesús David Monsalve Silva <Jdavidmonsalvesilva@gmail.com>
2025-09-12 17:00:29 -0500

    Tercer commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,22 +5,28 @@
 
 @section('content')
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="styleinicio.css">
 </head>
 <body>
  
-  <main>
-    <section id="bienvenida">
-      <h1>Barbería El Luchón </h1>
+ <div class="franja-blanca">
+<div id="bienvenida">
+    <img src="LOGO.png" alt="Barbería">
+    <div class="texto">
+        <h1>Barberia el Luchon</h1>
+        <p>El Luchón es una barbería moderna y auténtica donde cada corte se realiza con precisión y pasión, resaltando la personalidad de cada cliente. Aquí no solo te renuevas por fuera, sino que también sales motivado por dentro.</p>
+    </div>
+</div>
+
       <p>
-        El Luchón es más que una barbería, es un espacio donde se encuentran el estilo, la actitud y la buena vibra. 
-        Aquí cada corte se hace con precisión, pasión y dedicación, buscando siempre resaltar la personalidad de cada cliente.
-        Con un ambiente moderno y auténtico, en El Luchón no solo te renuevas por fuera, también sales motivado por dentro.
-        Ven, siéntete como en casa y luce como un campeón.
+        
       </p>
     </section>
     <section id="testimonios">
@@ -43,12 +49,30 @@
         <p>"Servicio rápido y con estilo. 10/10."</p>
         <span>— Andrés M.</span>
       </div>
+            <div class="testimonial">
+        <p>"Excelente atención y cortes impecables. ¡Volveré seguro!"</p>
+        <span>— Juan P.</span>
+      </div>
+      <div class="testimonial">
+        <p>"El ambiente es genial y los barberos súper profesionales."</p>
+        <span>— María G.</span>
+      </div>
+      <div class="testimonial">
+        <p>"Mi barba nunca ha lucido mejor. ¡Gracias El Luchón!"</p>
+        <span>— Carlos T.</span>
+      </div>
+      <div class="testimonial">
+        <p>"Servicio rápido y con estilo. 10/10."</p>
+        <span>— Andrés M.</span>
+      </div>
     </div>
   </div>
   
   
+  
+  
 </section>
-  </main>
+  
   </div>
 
 @endsection
